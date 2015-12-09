7dc9af60ba9d717a8e712c722cbe95809ae3c8b8
sam.mccord <sam.mccord@USMAC1872SMCOR.local>
2015-12-09 13:41:14 -0700

    Update model

--- server/api/stall/stall.model.js
+++ server/api/stall/stall.model.js
@@ -4,9 +4,16 @@ var mongoose = require('bluebird').promisifyAll(require('mongoose'));
 var Schema = mongoose.Schema;
 
 var StallSchema = new Schema({
-  name: String,
+  stallId: String,
   info: String,
-  active: Boolean
+  active: Boolean,
+  activity: Array
 });
+
+// Activities
+// {
+//   status : 'open',
+//   time : 12936790307
+// }
 
 module.exports = mongoose.model('Stall', StallSchema);
