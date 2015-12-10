4ebf3d0a3075af824024894bfe22d1931dd7f764
Craig Cogdill <craigmcogdill@gmail.com>
2015-12-10 14:25:03 -0700

    Updates with http post. Looks like hell for the time being but it work

--- python/interrupt.py
+++ python/interrupt.py
@@ -1,24 +1,75 @@
 #!/usr/bin/env python2.7
-
+import httplib, urllib
 import RPi.GPIO as GPIO
+import time
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+def stallPost(id, active):
+    params = urllib.urlencode({
+        'stallId' : id,
+        'active' : active
+        })
+    headers = {"Content-type": "application/x-www-form-urlencoded",
+               "Accept": "text/plain"}
+    conn = httplib.HTTPConnection("192.168.208.241:9000")
+    conn.request("POST", "/api/stalls",
+                 params, headers)
+    response = conn.getresponse()
+    print response.status, response.reason
+    data = response.read()
+    conn.close()
+
+door_1 = 19
+door_2 = 4
+
+
+GPIO.setup(door_1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(door_2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 
 def Callback(self):
-    if GPIO.input(5):
-        print "RISING Edge Detected"
+    if GPIO.input(door_1):
+        print "1: RISING Edge Detected"
+	stallPost(1, True)
     else:
-	print "FALLING Edge Detected"
+	print "1: FALLING Edge Detected"
+	stallPost(1, False)
+
+def Callback2(self):
+    if GPIO.input(door_2):
+        print "2: RISING Edge Detected"
+	stallPost(2, True)
+    else:
+	print "2: FALLING Edge Detected"
+	stallPost(2, False)
 
 
-GPIO.add_event_detect(5, GPIO.BOTH, callback=Callback, bouncetime=100)
+#GPIO.add_event_detect(door_1, GPIO.BOTH, callback=Callback, bouncetime=100)
+#GPIO.add_event_detect(door_2, GPIO.BOTH, callback=Callback2, bouncetime=100)
 
+GPIO.add_event_detect(door_1, GPIO.BOTH, callback=Callback)
+GPIO.add_event_detect(door_2, GPIO.BOTH, callback=Callback2)
 try:
+    #while True:
+    #    pass
+
     while True:
-        pass
+        time.sleep(10)
+
+
+
+
+
 except KeyboardInterrupt:
     GPIO.cleanup()
 
 GPIO.cleanup()
+
+
+
+
+
+
+
+
+
