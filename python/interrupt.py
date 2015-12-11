bea7e6917640d8f24ba737e25d62090812f8c0d4
Craig Cogdill <craigmcogdill@gmail.com>
2015-12-11 10:43:14 -0700

    Including some new logging and posting to the app

--- python/interrupt.py
+++ python/interrupt.py
@@ -2,8 +2,13 @@
 import httplib, urllib
 import RPi.GPIO as GPIO
 import time
+import logging
 from post import stallPost
 GPIO.setmode(GPIO.BCM)
+
+logging.basicConfig(filename='/var/log/lr.log',
+                    level=logging.DEBUG,
+                    format='%(asctime)s %(message)s')
 
 handicapped_stall = 24
 little_stall = 4
@@ -13,17 +18,19 @@ GPIO.setup(little_stall, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 def Callback(self):
     if GPIO.input(handicapped_stall):
-        print "1: RISING Edge Detected"
-	stallPost(1, True, False)
+	logging.info('Handicapped stall door closed')
+	stallPost(1, True, True)
     else:
-	print "1: FALLING Edge Detected"
-	stallPost(1, False, False)
+	logging.info('Handicapped stall door open')
+	stallPost(1, False, True)
 
 def Callback2(self):
     if GPIO.input(little_stall):
-	stallPost(2, True, True)
+	logging.info('Little stall door closed')
+	stallPost(2, True, False)
     else:
-	stallPost(2, False, True)
+	logging.info('Little stall door open')
+	stallPost(2, False, False)
 
 GPIO.add_event_detect(handicapped_stall, GPIO.BOTH, callback=Callback)
 GPIO.add_event_detect(little_stall, GPIO.BOTH, callback=Callback2)
